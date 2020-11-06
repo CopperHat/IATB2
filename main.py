dfd80975f79f00f205be588bef9573077e35bd8c
Ricardo Valencia <u201410873@upc.edu.pe>
2020-11-06 14:42:20 -0500

    UI CHanges
    Some UI improvements before working on rat

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ pg.display.set_caption('Inteligencia Artificial - Trabajo 2')
 size_cell = 50
 
 # Posicion del Laberinto
-posLabX = 200
+posLabX = 10
 posLabY = 150
 
 # Posicion del Raton
-posRatX = 210
-posRatY = 160
+posRatX = posLabX + 10
+posRatY = posLabY + 10
+# Posicion del Raton en Casilla
+mouseX = 0
+mouseY = 0
 
 # Posicion del Queso
-posCheX = 200
-posCheY = 150
+posCheX = posRatX
+posCheY = posRatY
 # Posicion del queso en Casilla
 cheseX = 6
 cheseY = 6
@@ -38,22 +41,25 @@ maze_array = [[1, 0, 1, 1, 1, 1, 0],
               [1, 1, 1, 1, 1, 0, 0],
               [1, 0, 0, 0, 0, 0, 1],
               [1, 0, 1, 1, 1, 1, 1],
-              [1, 1, 1, 0, 1, 1, 1]
-              ]
+              [1, 1, 1, 0, 1, 1, 1]]
+
 
 def draw_text(text, size, pos_x, pos_y, surface):
     font = pg.font.SysFont('comicsans', size)
     label = font.render(text, 1, (255, 255, 255))
     surface.blit(label, (pos_x, pos_y))
 
+
 def draw_mouse(surface, x, y):
     surface.blit(mouse, (posRatX + size_cell * x, posRatY + size_cell * y))
 
     pg.display.update()
 
+
 def draw_chesse(surface, x, y):
     surface.blit(chese, (posCheX + size_cell * x, posCheY + size_cell * y))
     pg.display.update()
+
 
 def draw_maze(surface):
     # Dibujar Fondo del Laberinto
@@ -68,22 +74,24 @@ def draw_maze(surface):
             if maze_array[i][j]:
                 pg.draw.rect(surface, (255, 255, 255),
                              (x + posLabX, y + posLabY, size_cell, size_cell))
+                pg.draw.rect(surface, (0, 0, 0),
+                             (x + posLabX, y + posLabY, size_cell, size_cell), 1)
     pg.display.update()
 
+
 def update_surface(surface, mouseX, mouseY):
-    draw_text('Trabajo 2', 100, 200, 50, surface)
+    surface.fill((228, 35, 34))
+    draw_text('Trabajo 2', 80, 10, 10, surface)
+    draw_text('Grupo 4', 50, 20, 70, surface)
     draw_maze(surface)
     draw_chesse(surface, cheseX, cheseY)
     draw_mouse(surface, mouseX, mouseY)
     pg.display.update()
 
+
 run = True
 victory = True
-# Maze dimensions (ncols, nrows)
-win.fill((150, 10, 100))
-# Posicion inicial de la Rata
-mouseX = 0
-mouseY = 0
+
 update_surface(win, mouseX, mouseY)
 while run:
     for event in pg.event.get():
